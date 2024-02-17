6b255fafae2a4df9374309f7a3a1caedc95675a8
Vafls <87562076+Vafls@users.noreply.github.com>
2024-02-17 12:03:16 +0200

    Update user.py

--- vLaunch/kernel/user.py
+++ vLaunch/kernel/user.py
@@ -1,2 +1,2 @@
 name = "Vafls" # replace 'Vafls' to your account name
-avatar = "C:\\vLaunch\\source\\avatar.png"
+avatar = "C:\\vLaunch\\source\\avatar.png" # replace avatar.png to your image with 32x32 resolution 
