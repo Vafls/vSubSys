4dd0bea9e78a208483a74024607b30fce54f45be
Vafls <87562076+Vafls@users.noreply.github.com>
2024-12-05 23:32:10 +0200

    OUTDATED
    unpacked version of the subsystem + older kernels included

---
+++ old_rev/vSubSys_plus_older_kernels/vLaunch/custom_kernel/multicore/boot/boot.py
@@ -0,0 +1,4 @@
+import subprocess
+
+core_path = r'C:\vLaunch\custom_kernel\multicore\kernel\core.py'
+subprocess.run(["python", core_path])
---
+++ old_rev/vSubSys_plus_older_kernels/vLaunch/main.py
@@ -0,0 +1,4 @@
+import subprocess
+
+boot_path = "C:\\vLaunch\\boot\\boot.py"
+subprocess.run(["python", boot_path])
